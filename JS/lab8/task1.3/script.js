6d6566026b7e75484b3b4be49f1cbde487fae7c7
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2022-11-26 21:07:51 +0200

    Fix: lab8 task 1.3

--- JS/lab8/task1.3/script.js
+++ JS/lab8/task1.3/script.js
@@ -7,17 +7,19 @@ function handleClick() {
   imgEl = win.document.createElement("img");
   imgVal = document.querySelector('input[name="name"]:checked').value;
   imgEl.src = imgVal;
-  imgEl.style.width = "100%";
-  imgEl.style.height = "100%";
+  imgEl.style.width = "80%";
+  imgEl.style.height = "80%";
 
-  let textEl = win.document.createElement("p");
-  let textVal = document.getElementById("txt").value;
-  textEl.textContent = textVal;
+  setTimeout(function () {
+    let textEl = win.document.createElement("p");
+    let textVal = document.getElementById("txt").value;
+    textEl.textContent = textVal;
 
-  win.document.body.appendChild(imgEl);
-  win.document.body.appendChild(textEl);
+    win.document.body.appendChild(imgEl);
+    win.document.body.appendChild(textEl);
 
-  console.log(win.document);
+    console.log(win.document);
+  }, 1000);
 }
 
 function handleClose() {
