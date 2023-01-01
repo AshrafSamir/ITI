2ad426a434c7814aaa464da4c791ba1ae6337a85
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2023-01-01 17:41:24 +0200

    task4 in lab3 css3 and html5

--- HTML5&CSS3/Lab3/Tasks/AllCanvasTasks/MovingRectangle/script.js
+++ HTML5&CSS3/Lab3/Tasks/AllCanvasTasks/MovingRectangle/script.js
@@ -1,41 +1,40 @@
 var canvas = document.getElementsByTagName("canvas")[0];
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "rgba(135, 206, 235, 1)";
-
-
+ctx.fillStyle = "rgba(135, 206, 235, 0.5)";
 
 ctx.beginPath();
-ctx.fillRect(150, 125, 200, 50);
+ctx.fillRect(250, 225, 200, 50);
 ctx.stroke();
 ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(150, 125, 5, 0, 2 * Math.PI);
-ctx.fillStyle = "blue";
-ctx.fill();
+// ctx.beginPath();
+// ctx.arc(250, 225, 5, 0, 2 * Math.PI);
+// ctx.fillStyle = "blue";
+// ctx.fill();
 
 ctx.closePath();
 
+let counter = 0;
+let value = 0.6;
 
+var inter = setInterval(function () {
+  counter++;
 
-ctx.beginPath();
-ctx.translate(150, 125);
-context.rotate(0.1);
-
-ctx.fillStyle = "rgba(135, 206, 235, 1)";
-ctx.fillRect(150, 125, 200, 50);
-ctx.stroke();
-ctx.closePath();
-
-
-
-
-
-
-
-
-
-
-
-
+  if (counter == 10) {
+    value = -0.6;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  } else {
+    ctx.beginPath();
+    ctx.translate(250, 225);
+    ctx.rotate(value);
+    ctx.translate(-250, -225);
+    ctx.fillStyle = "rgba(135, 206, 235, 0.5)";
+    ctx.fillRect(250, 225, 200, 50);
+    ctx.stroke();
+    ctx.closePath();
+  }
+  if (counter >= 20) {
+    clearInterval(inter);
+  }
+}, 500);
