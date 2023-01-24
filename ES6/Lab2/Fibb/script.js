c70e67cf933906076b7705c54e85513a68ab78f6
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2023-01-24 20:30:34 +0200

    Fix: fibbo task

--- ES6/Lab2/Fibb/script.js
+++ ES6/Lab2/Fibb/script.js
@@ -1,27 +1,35 @@
 function* fibbGen1(num) {
-  let res = 0;
+  let res = 1;
+  let lastRes = 0;
   for (let i = 0; i < num; i++) {
-    res = i < 2 ? i : res + i;
-    yield res;
+    if (i < 2) yield i;
+    else {
+      [res, lastRes] = [res + lastRes, res];
+      yield res;
+    }
   }
 }
 
 function* fibbGen2(maxNum) {
-  let res = 0;
-  let i = 0;
+  let res = 1;
+  let lastRes = 0,
+    i = 0;
   while (res < maxNum) {
-    res = i < 2 ? i : res + i;
+    if (i < 2) yield i;
+    else {
+      [res, lastRes] = [res + lastRes, res];
+      res < maxNum ? yield res : null;
+    }
     i++;
-    yield res;
   }
 }
 
-let fibb1 = fibbGen1(10);
-for (let i of fibb1) {
-  console.log(i);
-}
+// let fibb1 = fibbGen1(13);
+// for (let i of fibb1) {
+//   console.log(i);
+// }
 
-let fibb2 = fibbGen2(10);
+let fibb2 = fibbGen2(90);
 for (let i of fibb2) {
   console.log(i);
 }
